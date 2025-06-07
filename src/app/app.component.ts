040225112795e1228f3dc8a7b416687f7069f1ad
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-07 15:26:36 +1000

    photo card dsign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PhotoCard1Component } from './photo-card-1/photo-card-1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PhotoCard1Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = '30_Apps_Angular';
---
+++ src/app/photo-card-1/photo-card-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-card-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './photo-card-1.component.html',
+  styleUrl: './photo-card-1.component.scss'
+})
+export class PhotoCard1Component {
+
+}
