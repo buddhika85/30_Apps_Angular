b7c4bca19321cd8995406a82d7e59af86ea40e53
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-07 15:55:59 +1000

    photo card complete

--- src/app/photo-card-1/photo-card-1.component.ts
+++ src/app/photo-card-1/photo-card-1.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-photo-card-1',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './photo-card-1.component.html',
-  styleUrl: './photo-card-1.component.scss'
+  styleUrl: './photo-card-1.component.scss',
 })
-export class PhotoCard1Component {
+export class PhotoCard1Component implements OnInit {
+  user: UserDto = null!;
+  ngOnInit(): void {
+    this.user = { name: '', age: '', description: '' };
+  }
+}
 
+export interface UserDto {
+  name: string;
+  age: string;
+  description: string;
 }
