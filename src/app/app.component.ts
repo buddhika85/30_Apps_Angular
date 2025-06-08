bfb97931abf2bef872f9de5308d4931cfc47e280
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-08 13:23:05 +1000

    To Do List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PhotoCard1Component } from './photo-card-1/photo-card-1.component';
 import { Counter2Component } from './counter-2/counter-2.component';
+import { ToDoList3Component } from './to-do-list-3/to-do-list-3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PhotoCard1Component, Counter2Component],
+  imports: [
+    RouterOutlet,
+    PhotoCard1Component,
+    Counter2Component,
+    ToDoList3Component,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/to-do-list-3/to-do-list-3.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, Injectable } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-to-do-list-3',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './to-do-list-3.component.html',
+  styleUrl: './to-do-list-3.component.scss',
+})
+export class ToDoList3Component {
+  toDoList: ToDoList = inject(ToDoList);
+  toDoStr: string = '';
+
+  addToDo() {
+    this.toDoList.addToDo(this.toDoStr);
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+export class ToDoList {
+  toDos: ToDo[] = [];
+  private lastId: number = 0;
+
+  addToDo(toDo: string) {
+    this.toDos.push(new ToDo(++this.lastId, toDo));
+  }
+
+  removeToDo(id: number) {
+    const indexOfToDoToRemove = this.toDos.findIndex((x) => x.id === id);
+    this.toDos.splice(indexOfToDoToRemove, 1); // remove
+  }
+}
+
+export class ToDo {
+  id: number = 0;
+  description: string = '';
+
+  constructor(id: number, description: string) {
+    this.id = id;
+    this.description = description;
+  }
+
+  toString() {
+    return `${this.id} ${this.description}`;
+  }
+}
