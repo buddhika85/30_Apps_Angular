74a09b6cb903223e9d713d45bf514830b21ff5b2
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-21 10:07:05 +1000

    form changes

--- src/app/contact-form-13/contact-form-13.component.ts
+++ src/app/contact-form-13/contact-form-13.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form-13',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact-form-13.component.html',
-  styleUrl: './contact-form-13.component.scss'
+  styleUrl: './contact-form-13.component.scss',
 })
 export class ContactForm13Component {
+  contactInfo: Contact = { name: '', email: '', subject: '', message: '' };
 
+  onSubmit(): void {
+    console.log(this.contactInfo);
+  }
+}
+
+export interface Contact {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
 }
